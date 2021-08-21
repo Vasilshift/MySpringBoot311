c970bfcbb75323432f8317eb8f3795025322c95d
Evgeny <vasilkalin@gmail.com>
2021-08-21 10:31:11 +0200

    Work with initial method
    Класс MyUserDetailsService перенести в дир сервис
    В контроллерах сделать общий @RequestMapping для всех методов
    Убрать неиспользуемые импорты
    Почему нет интерфейсов у классов сервиса?
    Сделать метод или sql файл для начальной инициализации ролей, админа и юзера
    Убрать закомментированный код или перенести в нужное место(метод инициализации)

--- src/main/java/web/SpringbootdemoApplication.java
+++ src/main/java/web/SpringbootdemoApplication.java
@@ -1,7 +1,16 @@
 package web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import web.model.Role;
+import web.model.User;
+import web.repository.RoleRepository;
+import web.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
@@ -10,46 +19,41 @@ public class SpringbootdemoApplication {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
-//	private UserService userService;
-//	private BCryptPasswordEncoder bcryptpasswordEncoder;
-//	private RoleRepository roleRepository;
-//
-//	@Autowired
-//	public SpringbootdemoApplication(UserService userService, BCryptPasswordEncoder bcryptpasswordEncoder, RoleRepository roleRepository) {
-//		this.userService = userService;
-//		this.bcryptpasswordEncoder = bcryptpasswordEncoder;
-//		this.roleRepository = roleRepository;
-//	}
-//
-//
-//
-//	@PostConstruct
-//	void init(){
-//
-//		Role roleAdmin = new Role();
-//		roleAdmin.setId(1L);
-//		roleAdmin.setName("ROLE_ADMIN");
-//		roleRepository.save(roleAdmin);
-//
-//		Role roleUser = new Role();
-//		roleUser.setId(2L);
-//		roleUser.setName("ROLE_USER");
-//		roleRepository.save(roleUser);
-//
-//		User user1 = new User();
-//		user1.setUsername("admin");
-//		user1.setPassword(bcryptpasswordEncoder.encode("admin"));
-//		user1.setRoles(Collections.singleton(roleRepository.findRoleByName("ROLE_ADMIN")));
-//		userService.saveUser(user1);
-//
-//		User user2 = new User();
-//		user2.setUsername("user");
-//		user2.setPassword(bcryptpasswordEncoder.encode("user"));
-//		user2.setRoles(Collections.singleton(roleRepository.findRoleByName("ROLE_USER")));
-//		userService.saveUser(user2);
-//	}
+	private UserService userService;
+	private BCryptPasswordEncoder bcryptpasswordEncoder;
+	private RoleRepository roleRepository;
 
+	@Autowired
+	public SpringbootdemoApplication(UserService userService, BCryptPasswordEncoder bcryptpasswordEncoder, RoleRepository roleRepository) {
+		this.userService = userService;
+		this.bcryptpasswordEncoder = bcryptpasswordEncoder;
+		this.roleRepository = roleRepository;
+	}
 
+	@PostConstruct
+	void init(){
 
+		Role roleAdmin = new Role();
+		roleAdmin.setId(1L);
+		roleAdmin.setName("ROLE_ADMIN");
+		roleRepository.save(roleAdmin);
+
+		Role roleUser = new Role();
+		roleUser.setId(2L);
+		roleUser.setName("ROLE_USER");
+		roleRepository.save(roleUser);
+
+		User user1 = new User();
+		user1.setUsername("admin");
+		user1.setPassword(bcryptpasswordEncoder.encode("admin"));
+		user1.setRoles(Collections.singleton(roleRepository.findRoleByName("ROLE_ADMIN")));
+		userService.saveUser(user1);
+
+		User user2 = new User();
+		user2.setUsername("user");
+		user2.setPassword(bcryptpasswordEncoder.encode("user"));
+		user2.setRoles(Collections.singleton(roleRepository.findRoleByName("ROLE_USER")));
+		userService.saveUser(user2);
+	}
 
 }
